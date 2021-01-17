e0cbb2f6c9ee93617688427d496a0c67db94bc97
luisdelgado <luishdelgados10@gmail.com>
2021-01-17 20:31:09 -0300

    feat(home): exibindo ÚLTIMOS EDITAIS ADICIONADOS

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface Edict {
+  name: string;
+  closingDate: string;
+  progress: string;
+  pendencies: string
+}
+
+const ELEMENT_DATA: Edict[] = [
+  {name: 'Klabin Transforma - Banco de Projetos', closingDate: '31/05/21', progress: "80%", pendencies: 'justificativa, currículo'},
+  {name: 'EDITAL FUNDO NOSSA PARTE PELO TODO IBRF 2021', closingDate: '31/05/21', progress: "80%", pendencies: 'justificativa, currículo'}
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  displayedColumns: string[] = ['name'];
+  dataSource = ELEMENT_DATA;
 
   constructor(
     private router: Router
